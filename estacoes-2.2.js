5a9ed4ee6c0427aca569696a6d293b65f6d7965e
energybrasil-fm <energybrasil98@gmail.com>
2026-09-06 17:54:35 -0300

    Update estacoes-2.2.js
    Updated

--- estacoes-2.2.js
+++ estacoes-2.2.js
@@ -10,8 +10,8 @@
 export const stations = [
     {
         "id": "Dublin's_brasil",
-        "name": "Dublin's Brasil 98.FM",
-        "description": "A Top 40 do Brasil",
+        "name": "Dublin's Brasil 98 FM",
+        "description": "O Top 40 do Brasil e do mundo",
         "logotipo": "https://www.98fm.com/images/red-logo.svg",
         "api": "https://az11.yesstreaming.net:8300",
         "historyApi": "",
@@ -21,8 +21,8 @@ export const stations = [
         "limitHistory": 20,
         "contact": "https://wa.me/559191930858?text=Olá,%20preciso%20de%20atendimento!",
         "visualizer": "true",
-        "defaultArt": "https://assets.radioplayer.org/372/37218/600/600/la2q4l57.png",
-        "bgdefaultArt": "https://assets.radioplayer.org/372/37218/600/600/la2q4l57.png",
+        "defaultArt": "https://energybrasil.xtgem.com/app_web.jpg",
+        "bgdefaultArt": "http://energybrasil.xtgem.com/app_web.jpg",
         "streams": {
             "high": { "url": "", "format": "Alta qualidade" },
             "mid": { "url": "https://az11.yesstreaming.net/listen/dublins_brasil_98fm/dublinbrasil.mp3", "format": "Qualidade padrão" },
