5b7381d215f53ccad5e2f6941e2bb0a2dc20c302
energybrasil-fm <energybrasil98@gmail.com>
2026-09-09 19:15:53 -0300

    Update and rename script-3.3.js to script-3.4.js
    Update v3.4

--- script-3.4.js
+++ script-3.4.js
@@ -1,4 +1,4 @@
-import { stations } from './estacoes-2.5.js';
+import { stations } from './estacoes-2.6.js';
         import { updateIntervalTime, isValidTrack, fetchItunesData, fetchStationData, fetchLyrics } from './api_config-2.0.js';
 
 // ==========================================
